f0dfcab2b3df5550180b7fe9191c495f0de76747
tsabarinoam <tsabarinoam@gmail.com>
2025-07-28 00:45:14 +0000

    Edited Spark

--- src/components/SparkIntegrationSetup.tsx
+++ src/components/SparkIntegrationSetup.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Badge } from '@/components/ui/badge'
 import { Alert, AlertDescription } from '@/components/ui/alert'
-  Termina
   Rocket,rcle,
   Download,
 import { 
